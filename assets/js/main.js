4339c7bb342044047d41fd096b769b8758c06b66
fatihyanik <fatihyanik07@gmail.com>
2022-11-20 11:14:02 +0100

    remove show menu

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+/* SHOW MENU */
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*Menu Show*/
+/*Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', ()=>{
+        navMenu.classList.add('show-menu')
+    })
+}     
+
+/*Menu Hidden */
+/*Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu')
+    })
+} 
